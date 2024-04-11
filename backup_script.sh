03a329719c00c3d0d13f3f170224008ac512f80a
rinshad-ablip <rinshadnazar077@gmail.com>
2024-04-11 20:09:11 +0530

    backup include only file backup

---
+++ backup_script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# set variables for remote server
+REMOTE_USER="remote_user"
+REMOTE_HOST="remote.host.com"
+REMOTE_DIR="/path/to/backup/folder"
+
+# set variables for database backup
+DB_USER="database_user"
+DB_PASS="database_password"
+DB_NAME="database_name"
+
+# set variables for local backup
+LOCAL_DIR="/path/to/local/backup/folder"
+BACKUP_NAME="$(date +%Y%m%d%H%M%S)_backup.tar.gz"
+
+# create backup folder on local machine
+mkdir -p $LOCAL_DIR
+
+# ssh into remote server and create backup archive
+ssh $REMOTE_USER@$REMOTE_HOST "cd $REMOTE_DIR && tar czf - $DB_NAME.sql | gzip -c" > $LOCAL_DIR/$BACKUP_NAME
+
+echo "Backup completed successfully"
